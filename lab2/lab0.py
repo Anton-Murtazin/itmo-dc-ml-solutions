e767bcaa4e5b1324d9b16c9156c3a4e3ead3fb5b
Anton-Murtazin <114882220+Anton-Murtazin@users.noreply.github.com>
2024-10-20 20:04:00 +0300

    Update lab0.py

--- lab2/lab0.py
+++ lab2/lab0.py
@@ -5,12 +5,12 @@ df.info()
 
 # Представим ситуацию, что из-за невнимательности операциониста, регионы: 
 # Республика Алтай, Магаданская обл. оказались не представлены в итоговой сводке.
-df.drop(['Республика Алтай', 'Магаданская обл.'], inplace=True)
+df.drop(['Нижегородская область', 'Кемеровская область', 'Приморский край', 'Ненецкий АО', 'Самарская область'], inplace=True)
 df.sort_values('salary', inplace=True)
 
-print(df.head(6).tail(1))
-print(df.head(49).tail(1))
-print(df.head(51).tail(1))
+print(df.head(24).tail(1))
+print(df.head(38).tail(1))
+print(df.head(62).tail(1))
 
 print(df['salary'].mean())
 print(df['salary'].median())
